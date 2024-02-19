a7642bd698a26af003006b585965dd6e42ecd42e
Antonio Colaierà <colaiera@gmail.com>
2024-02-19 09:33:41 +0100

    Spring boot app

---
+++ CharactersSocialNetwork.Server/Characters-Social-Network/src/main/java/com/characters/CharactersSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.characters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharactersSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharactersSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ CharactersSocialNetwork.Server/Characters-Social-Network/src/test/java/com/characters/CharactersSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.characters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharactersSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
