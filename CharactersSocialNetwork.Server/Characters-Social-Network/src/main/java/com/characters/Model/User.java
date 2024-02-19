cccc6f277a92312a739e2d50d5921cacb5f0d8a9
Antonio Colaierà <colaiera@gmail.com>
2024-02-19 14:04:42 +0100

    React app

---
+++ CharactersSocialNetwork.Server/Characters-Social-Network/src/main/java/com/characters/Model/User.java
@@ -0,0 +1,38 @@
+package com.characters.Model;
+
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String userId;
+    
+    @Column(nullable = false)
+    private String username;
+    
+    @Column(nullable = false)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    private String bio;
+    
+    @Column(name = "profile_picture_url")
+    private String profilePictureUrl;
+    
+    @OneToMany(mappedBy = "user")
+    private List<User> friends;
+    
+    // @OneToMany(mappedBy = "user")
+    // private List<Post> posts;
+}
