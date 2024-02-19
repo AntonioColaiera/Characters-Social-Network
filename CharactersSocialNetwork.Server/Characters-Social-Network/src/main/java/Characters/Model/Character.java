2185d87a4eede8e51a1bcfd221f94ca168c2af40
AntonioColaiera <colaiera@gmail.com>
2024-02-19 15:36:43 +0100

    back end Spring boot

--- CharactersSocialNetwork.Server/Characters-Social-Network/src/main/java/Characters/Model/Character.java
+++ CharactersSocialNetwork.Server/Characters-Social-Network/src/main/java/Characters/Model/Character.java
@@ -1,5 +1,32 @@
 package Characters.Model;
 
+import jakarta.persistence.*;
+import lombok.Data; 
+import lombok.NoArgsConstructor; 
+import lombok.AllArgsConstructor; 
+
+@Entity
+@Table(name = "characters") 
+@Data 
+@NoArgsConstructor 
+@AllArgsConstructor 
 public class Character {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String characterId;
+    
+    @Column(nullable = false)
+    private String title;
+    
+    @Column(nullable = false)
+    private String content;
+    
+    @Column(name = "photo")
+    private String photo;
+    
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable=false)
+    private User user;
+    
 
 }
